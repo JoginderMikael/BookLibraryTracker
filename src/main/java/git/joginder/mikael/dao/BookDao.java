2e0ffae5f0f05f6b23d4b63dd94164270ca411d1
Joginder Mikael <jogindermikael@gmail.com>
2025-11-07 00:15:31 +0300

    getBook by ID.

--- src/main/java/git/joginder/mikael/dao/BookDao.java
+++ src/main/java/git/joginder/mikael/dao/BookDao.java
@@ -5,6 +5,7 @@ import git.joginder.mikael.util.DBConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -23,14 +24,31 @@ public class BookDao {
             ps.setString(2, book.getAuthor());
             ps.setInt(3, book.getYear());
             ps.setString(4, book.getIsbn());
-            ps.executeUpdate();
+            ps.executeUpdate(); //executeUpdate() for INSERT, UPDATE, DELETE
         }catch(SQLException e){
             IO.println("FAILED. " +e.getMessage());
         }
     }
 
     public Book getBookById(int id){
+        String sql = "SELECT * FROM books WHERE id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setInt(1, id);
 
+            try(ResultSet rs = ps.executeQuery()){ //executeQuery() for SELECT
+                if(rs.next()){
+                    Book book = new Book();
+                    book.setId(rs.getInt("id"));
+                    book.setTitle(rs.getString("title"));
+                    book.setAuthor(rs.getString("author"));
+                    book.setYear(rs.getInt("year"));
+                    book.setIsbn(rs.getString("isbn"));
+                    return book;
+                }
+            }
+        }catch(SQLException e){
+            IO.println("FAILED. " + e.getMessage());
+        }
         return null;
     }
 
