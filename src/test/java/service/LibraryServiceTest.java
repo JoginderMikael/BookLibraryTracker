14270fdfead97499486f372c41c257a579f785a2
Joginder Mikael <jogindermikael@gmail.com>
2025-11-11 16:39:18 +0300

    Library service Tests

--- src/test/java/service/LibraryServiceTest.java
+++ src/test/java/service/LibraryServiceTest.java
@@ -1,10 +1,14 @@
 package service;
 
 import git.joginder.mikael.dao.BookDao;
+import git.joginder.mikael.model.Book;
 import git.joginder.mikael.service.LibraryService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LibraryServiceTest {
 
@@ -18,7 +22,11 @@ public class LibraryServiceTest {
 
     @Test
     void testValidationLogic(){
-
+        Book invalidBook = new Book(0, null, "Mike", 2024, "273838");
+        assertEquals(false, libraryService.addBook(invalidBook));
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            libraryService.addBook(invalidBook);
+//        });
     }
 
     @Test
