0b5555167bd1238e692f9fafd5ea2e678faf5663
Joginder Mikael <jogindermikael@gmail.com>
2025-11-11 16:21:20 +0300

    BookDao Tests

--- src/test/java/dao/BookDaoTest.java
+++ src/test/java/dao/BookDaoTest.java
@@ -1,12 +1,16 @@
 package dao;
 
 import git.joginder.mikael.dao.BookDao;
+import git.joginder.mikael.model.Book;
 import git.joginder.mikael.util.DBConnection;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookDaoTest {
 
@@ -21,22 +25,33 @@ public class BookDaoTest {
     @Test
     void testAddBook(){
 
+        Book book = new Book();
+        book.setTitle("The Year of Learning");
+        book.setYear(2025);
+        book.setIsbn("985993847465");
+        assertDoesNotThrow(() -> {
+            bookDao.addBook(book);
+        });
     }
     @Test
     void testGetBookById(){
-
+        assertNotNull(bookDao.getBookById(21));
     }
     @Test
     void testUpdateBook(){
-
+        Book book = new Book();
+        book.setId(21);
+        book.setAuthor("Joginder Mk1");
+        boolean b = bookDao.updateBook(book);
+        assertTrue(b);
     }
     @Test
     void testDeleteBook(){
-
+        assertTrue(bookDao.deleteBook(21));
     }
     @AfterEach
-    void tearDown(){
-
+    void tearDown() throws SQLException {
+        dbConnection.close();
     }
 
 }
